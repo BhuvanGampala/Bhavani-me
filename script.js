dfe30b695049a2b7877c16ddd32fc68a37ca437a
BhuvanGampala <147086668+BhuvanGampala@users.noreply.github.com>
2024-08-08 22:00:08 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const greetingContainer = document.getElementById('greeting-container');
+
+    // Show the greeting for 5 seconds, then fade it out
+    setTimeout(() => {
+        greetingContainer.style.opacity = '0';
+        setTimeout(() => {
+            greetingContainer.style.display = 'none';
+        }, 1000); // Wait for the fade-out transition to complete
+    }, 5000); // Display the greeting for 5 seconds
+
     const months = {
-        "January": ["gruhapravesh.jpg", "BHUVAN.jpg"],
-        "February": ["pancard.jpg", "image4.jpg"],
+        "June 2023": ["gruhapravesh.jpg"],
+        "July": ["july23.jpg"],
+        "August": ["aug23.jpg"],
+        "September": ["sep1.jpg", "sep2.jpg"],
+        "October": ["oct1.jpg", "oct2.jpg","oct3.jpg"],
+        "November": ["nov1.jpg", "nov2.jpg","nov3.jpg", "nov4.jpg"],
+        "December": ["dec1.jpg", "dec2.jpg","dec3.jpg", "dec4.jpg"],
+        "January 2024": ["jan1.jpg", "jan2.jpg"],
+        "February": ["feb1.jpg", "feb2.jpg","feb3.jpg", "feb4.jpg","feb5.jpg", "feb6.jpg"],
+        "March": ["mar1.jpg", "mar2.jpg"],
+        "April": ["apr1.jpg", "apr2.jpg","apr3.jpg", "apr4.jpg","apr5.jpg", "apr6.jpg","apr7"],
+        "May": ["may1.jpg","may2.jpg"],
+        "June": ["jun1.jpg", "jun2.jpg"],
+        "July": ["jul1.jpg"],
+        "August": ["aug1.jpg"],
         // Add more months and photos as needed
     };
 
